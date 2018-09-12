188a569642864cd99f24f6fb72e6725c237c09a9
Daniel Hintz <danielchintz@gmail.com>
2018-09-12 15:37:35 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+javascript: 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+async function doit() {
+    while (1) {
+        await sleep(100);
+        document.getElementsByClassName('button Lts($ls-s) Z(0) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) recsGamepad__button D(b) Bgc(#fff) Wc($transform) recsGamepad__button--like Scale(1.1):h')[0].click();
+    }
+}
+doit()
